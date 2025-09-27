ba2a6891053c85b392ced2735efaa179866ac055
ihsn525 <ihsansiju456@gmail.com>
2025-09-27 22:33:16 +0530

    Add README and sample skills screen

---
+++ hydrohive/lib/features/skills/presentation/skills_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SkillsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Skill Pathways')),
+      body: Center(child: Text('Welcome to HydroHive!')),
+    );
+  }
+}
